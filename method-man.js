9b97ef53d3e19115fd59236c585ff45eb3168612
hokwach <hezronokwach@gmail.com>
2024-08-26 18:01:11 +0300

    Implement words, sentence, yell, whisper, and capitalize functions - words(): Splits a string into an array of words - sentence(): Joins an array of words into a sentence - yell(): Converts a string to uppercase - whisper(): Converts a string to lowercase - capitalize(): Capitalizes the first letter and makes the rest lowercase

---
+++ method-man.js
@@ -0,0 +1,20 @@
+function words(args) {
+    return args.split(' ')
+}
+
+function sentence(args) {
+    return args.join(' ')
+}
+
+function yell(args){
+    return args.toUpperCase()
+}
+
+function whisper(args){
+    return args.toLowerCase()
+}
+
+function capitalize(args){
+    return args[0].toUpperCase() + args.slice(1).toLowerCase()    
+}
+
