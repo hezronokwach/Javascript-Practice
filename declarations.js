d4e60f5362965bb197808f886df8d21342cd3b4f
hokwach <hezronokwach@gmail.com>
2024-08-26 12:04:54 +0300

    Add constant variables containing special characters Add array containing 2 values Add object Add nested object containing array and an object

---
+++ declarations.js
@@ -0,0 +1,20 @@
+const escapeStr = "`\\\/\"\'";
+const arr = [4, '2'];
+Object.freeze(arr);
+const obj = {
+    "str": "My string",
+    "num": 4,
+    "bool": true,
+    "undef": undefined
+};
+Object.freeze(obj);
+const nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        "str": "Hello js",
+        "num": 4,
+        "bool": true
+    }
+};
+Object.freeze(nested);
+
