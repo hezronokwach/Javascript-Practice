d97661824b45118152b987c5497eba596fbbd523
hokwach <hezronokwach@gmail.com>
2024-08-26 12:36:25 +0300

    Copy contents of the person to clone1 and clone2 Add samePerson variable of type person and change the age and country

--- mutability.js
+++ mutability.js
@@ -6,6 +6,6 @@ const person = {
 const clone1 = JSON.parse(JSON.stringify(person));
 const clone2 = JSON.parse(JSON.stringify(person));
 
-const samePerson = person;
+let samePerson = person;
 person.age += 1;
 person.country = 'FR';
