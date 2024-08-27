4d50b0d57fe13f28a26dc2eb5a28cbf37ec62013
hokwach <hezronokwach@gmail.com>
2024-08-27 09:18:16 +0300

    Add functions to get the maximum and minimum number when given two numbers

---
+++ min-max.js
@@ -0,0 +1,13 @@
+function max(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    }
+    return num2
+}
+
+function min(num1, num2) {
+    if (num1 < num2) {
+        return num1
+    }
+    return num2
+}
