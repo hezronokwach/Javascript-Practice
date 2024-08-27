c84c1498ce7b8894b4399c70e9936983400c3238
hokwach <hezronokwach@gmail.com>
2024-08-27 16:29:32 +0300

    Add more condition in the object check

--- collections.js
+++ collections.js
@@ -8,28 +8,28 @@ const mapToObj = value => Object.fromEntries(value.entries());
 const objToArr = value => Object.values(value);
 const objToMap = value => new Map(Object.entries(value));
 const arrToObj = value => Object.assign({}, value);
-const strToObj = value =>Object.assign({},strToArr(value));
+const strToObj = value => Object.assign({}, strToArr(value));
 
-function superTypeOf(value){
-    if (value instanceof Map){
+function superTypeOf(value) {
+    if (value instanceof Map) {
         return "Map";
-    } else if (value instanceof Set){
+    } else if (value instanceof Set) {
         return "Set";
-    }else if (value instanceof Object) {
+    } else if (value instanceof Object && value !== null && !(Array.isArray(value)) && !(typeof value === "function")) {
         return 'Object';
-    }else if (value === null) {
+    } else if (value === null) {
         return 'null';
-    } else if (typeof value === "string"){
+    } else if (typeof value === "string") {
         return 'String';
-    }else if (typeof value === "number" || "NaN"){
+    } else if (typeof value === "number" || "NaN") {
         return 'Number';
-    } else if (Array.isArray(value)){
+    } else if (Array.isArray(value)) {
         return 'Array';
-    }else if (typeof value === "undefined"){
+    } else if (typeof value === "undefined") {
         return 'undefined';
-    }else if(typeof value === "function"){
+    } else if (typeof value === "function") {
         return 'Function'
     }
 }
-console.log(strToSet("`12${ctx[0]}23`"))
-
+console.log(superTypeOf([]))
+value !== null && !is.arr(value) && !is.fun(value) 
