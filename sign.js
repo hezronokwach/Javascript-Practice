ec312cec234f46538c0adfe20345ca9df8b225f4
hokwach <hezronokwach@gmail.com>
2024-08-27 09:34:18 +0300

    Add function that checks if a number is positive or negative or 0 Add function to check if two numbers have the same sign

---
+++ sign.js
@@ -0,0 +1,20 @@
+function sign(num) {
+    if (num < 0) {
+        return -1
+    }
+    if (num > 0) {
+        return 1
+    }
+    if (num === 0) {
+        return 0
+    }
+}
+
+function sameSign(num1, num2){
+    if (num1 < 0 && num2 < 0){
+        return true
+    } else if (num1 >= 0 && num2 >= 0){
+        return true
+    } 
+    return false
+}
