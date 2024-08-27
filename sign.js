392fbcd1ffdb1cc70a62cb765c67fe2bc98c0dc0
hokwach <hezronokwach@gmail.com>
2024-08-27 09:37:57 +0300

    refactor: Check if both numbers are 0 and return true in the sameSign function

--- sign.js
+++ sign.js
@@ -13,8 +13,10 @@ function sign(num) {
 function sameSign(num1, num2){
     if (num1 < 0 && num2 < 0){
         return true
-    } else if (num1 >= 0 && num2 >= 0){
+    } else if (num1 > 0 && num2 > 0){
         return true
-    } 
+    } else if (num1 == 0 && num2 == 0){
+        return true
+    }
     return false
 }
