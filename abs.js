51fb9745001263c46994dc164d8ae1b1448f84d2
hokwach <hezronokwach@gmail.com>
2024-08-27 08:51:17 +0300

    Add function to check if a number is positive Add function to return absolute value of a number

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(num) {
+    if (num > 0) {
+        return true
+    }
+    return false
+}
+
+function abs(num) {
+    if (num < 0) {
+        return -num
+    }
+    if (num == -Infinity){
+        return Infinity
+    }
+}
