57417a8eb926b2178fdb532a493e8ae63c7db762
hokwach <hezronokwach@gmail.com>
2024-08-27 15:34:23 +0300

    Add functions that convert data from one type to another Add function that returns specific values for advanced data types

---
+++ collections.js
@@ -0,0 +1,32 @@
+const arrToSet = (value) => new Set(value);
+const arrToStr = (value) => value.toString();
+const setToArr = (value) => [...value];
+const setToStr = (value) => [...value].join(' ');
+const strToArr = (value) => value.split(" ");
+const mapToObject = (value) => Object.fromEntries(value.entries());
+const objToArr = (value) => Object.values(value);
+const objToMap = (value) => new Map(Object.entries(value));
+const arrToObj = (value) => Object.entries(value);
+const strToObj = value =>Object.entries(strToArr(value));
+
+function superTypeOf(value){
+    if (value instanceof Map){
+        return "Map";
+    } else if (value instanceof Set){
+        return "Set";
+    }else if (value instanceof Object) {
+        return 'Object';
+    }else if (value === null) {
+        return 'null';
+    } else if (typeof value === "string"){
+        return 'String';
+    }else if (typeof value === "number" || "NaN"){
+        return 'Number';
+    } else if (Array.isArray(value)){
+        return 'Array';
+    }else if (typeof value === "undefined"){
+        return 'undefined';
+    }else if(typeof value === "function"){
+        return 'Function'
+    }
+}
