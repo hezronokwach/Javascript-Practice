299ccaefbb82f671244a6cb42e24232a7cdfadae
hokwach <hezronokwach@gmail.com>
2024-08-28 09:49:34 +0300

    refactor:Change the logic of blockchain function

--- block-chain.js
+++ block-chain.js
@@ -1,59 +1,50 @@
-function blockChain(data, prev){
-    const prevBlock = prev || {
-        
-            index: 0,
-            hash : "0",
-            data : {},
-            prev : null,
-            chain : function(newData){
-                return blockChain(newData,this)
-            }
-        
-        }
-    
-    const index = prevBlock.index + 1;
-    const concatData = index + prevBlock.hash + JSON.stringify(data);
-    const newBlock = {
-        index: index,
-        hash : hashCode(concatData),
-        data : data,
-        prev : prevBlock,
-        chain : function(newData){
-            return blockChain(newData,this)
-        }
-
+function blockChain(data, prev) {
+    if (!prev) {
+        prev = { index: 0, hash: '0' };
     }
-    return newBlock;
+    var block = {
+        data,
+        prev,
+    };
+
+    block.index = block.prev.index + 1;
+    const concatData = block.index + prev.hash + JSON.stringify(data);
+    block.hash = hashCode(concatData);
+    block.chain = (data) => blockChain(data, block);
+    return block;
 }
 
 const hashCode = str =>
     (
-      [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
-    ).toString(36)
+        [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
+    ).toString(36);
 
-function chain(data){
 
-}
- const first = blockChain({ a: 1 })
+const first = blockChain({ a: 1 })
+console.log(first.index) //           -> 1
+console.log(first.data) //            -> { a: 1 }
+console.log(first.prev) //            -> { index: 0, hash: "0" }
+console.log(first.hash) //            -> '1103f27'
+console.log(hashCode('10{"a":1}')) // -> '1103f27'
 
 
 const second = first.chain({ hello: 'world' })
-// console.log(second.hash) //                           -> '18drvvc'
-// console.log(hashCode('21103f27{"hello":"world"}')) // -> '18drvvc'
+console.log(second.hash) //                           -> '18drvvc'
+console.log(hashCode('21103f27{"hello":"world"}')) // -> '18drvvc'
 
- const chain2 = second
-  .chain({ value: 4455 })
-  .chain({ some: 'data' })
-  .chain({ cool: 'stuff' })
+const chain = second
+    .chain({ value: 4455 })
+    .chain({ some: 'data' })
+    .chain({ cool: 'stuff' })
 
 const fork = second
-  .chain({ value: 335 })
-  .chain({ some: 'data' })
-  .chain({ cool: 'stuff' })
+    .chain({ value: 335 })
+    .chain({ some: 'data' })
+    .chain({ cool: 'stuff' })
 
-console.log(chain2.hash) //  -> '1qr3qfs'
+console.log(chain.hash) //  -> '1qr3qfs'
 console.log(fork.hash) //   -> '1x9gsc1'
-console.log(chain2.index) // -> 5
+console.log(chain.index) // -> 5
 console.log(fork.index) //  -> 5
 
 
