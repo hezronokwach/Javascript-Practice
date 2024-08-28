9a25f25195cfe389e3583c7ba56e92846b14fb1f
hokwach <hezronokwach@gmail.com>
2024-08-28 19:41:14 +0300

    Add slice function to  slice array from the start to end index

---
+++ slicer.js
@@ -0,0 +1,28 @@
+const slice = (value, start, end) => {
+    let resultArray = [];
+    let resultStr = "";
+    if (end == undefined){
+        end = value.length-1;
+    }
+    if (end < 0 ){
+        end = end + value.length-1;        
+    }
+    if (start < 0){
+        start = start + value.length-1;
+    }
+    if (Array.isArray(value)){
+        for (let i = start; i < end; i++){
+            resultArray.push(value[i])
+        }
+        return resultArray
+
+    } 
+    if (typeof value === "string"){
+        for (let i = start; i < end; i++){
+            resultStr += value[i]
+        }
+        return resultStr
+    }    
+}
+
+
