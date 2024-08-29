cb8ea457b193f37b8e60cd6b744bec8549860058
hokwach <hezronokwach@gmail.com>
2024-08-29 09:36:26 +0300

    Change the logic in cutfirstLast function

--- keep-cut.js
+++ keep-cut.js
@@ -1,13 +1,19 @@
 const cutFirst = (str) => str.substring(2);
 const cutLast = (str) => str.substring(0, str.length - 2);
-const cutFirstLast = (str) => str.substring(2,str.length-2);
+const cutFirstLast = (str) => {
+    if (str.length <= 4) {
+        return ""
+    } else {
+        return str.substring(2, str.length - 2);
+    }
+}
 const keepFirst = (str) => str.substring(0, 2);
 const keepLast = (str) => str.substring(str.length - 2, str.length);
 const keepFirstLast = (str) => {
-    if (str.length < 4){
+    if (str.length < 4) {
         return ""
-    }else{
+    } else {
         return keepFirst(str) + keepLast(str);
     }
 }
-console.log(keepFirstLast('af'))
+console.log(cutFirstLast('af'))
