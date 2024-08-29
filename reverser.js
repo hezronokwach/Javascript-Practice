c876948cc73761474f3f28275471ee5362741c3d
hokwach <hezronokwach@gmail.com>
2024-08-29 17:02:51 +0300

    Change the logic of the reverser function

--- reverser.js
+++ reverser.js
@@ -1,17 +1,12 @@
 const reverse = (value) => {
     let result = [];
-    let resultStr = ""
-    if (Array.isArray(value)){
-        for (let i = value.length-1; i >= 0; i-- ){
-            result.push(value[i])
-        }
-        return result
-    } else if (typeof value === 'string'){
-        for (let i = value.length-1; i >= 0; i-- ){
-            resultStr += value[i]
-        }
-        return resultStr
-    }  
-}
-
-console.log(reverse('pouet'))
+    
+    for (let i = value.length - 1; i >= 0; i--) {
+      result.push(value[i]);
+    }
+    
+    return typeof value === 'string' ? result.join('') : result;
+  };
+  
+  console.log(reverse('pouet'));  // Output: 'teuop'
+  console.log(reverse([1, 2, 3, 4, 5]));  // Output: [5, 4, 3, 2, 1]
