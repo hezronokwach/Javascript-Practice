fcf1568f626785e7298c05af83de2a7d8e630201
hokwach <hezronokwach@gmail.com>
2024-08-29 09:29:36 +0300

    refactor: Change the method of slicing in cutFirstLast function

--- keep-cut.js
+++ keep-cut.js
@@ -1,6 +1,7 @@
 const cutFirst = (str) => str.substring(2);
 const cutLast = (str) => str.substring(0, str.length - 2);
-const cutFirstLast = (str) => cutFirst(str) + cutLast(str);
+const cutFirstLast = (str) => str.substring(2,str.length-2);
 const keepFirst = (str) => str.substring(0, 2);
 const keepLast = (str) => str.substring(str.length - 2, str.length);
 const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
+console.log(keepFirstLast('abcdef'))
