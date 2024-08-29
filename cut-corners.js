8bb4bffa84a9acf091e43ef1a837c2bab03b37de
hokwach <hezronokwach@gmail.com>
2024-08-29 14:08:49 +0300

    Add round function

---
+++ cut-corners.js
@@ -0,0 +1,83 @@
+const multiply = (a, b) => {
+    let result = 0;
+    let absA = Math.abs(a)
+    let absB = Math.abs(b)
+    let isNegative = false;
+    if (a < 0 && b >= 0) {
+        isNegative = true;
+    } else if (a >= 0 && b < 0) {
+        isNegative = true;
+    }
+    while (absB > 0) {
+        result += absA
+        absB--
+    }
+    if (isNegative) {
+        return -result
+    } else {
+        return result
+    }
+}
+
+const divide = (a, b) => {
+    let result = 0;
+    let absA = Math.abs(a)
+    let absB = Math.abs(b)
+    let isNegative = false;
+    if (a < 0 && b >= 0) {
+        isNegative = true;
+    } else if (a >= 0 && b < 0) {
+        isNegative = true;
+    }
+    while ((absA - absB) >= 0) {
+        result++
+        absA -= absB
+    }
+    if (isNegative) {
+        return -result
+    } else {
+        return result
+    }
+}
+
+const modulo = (a, b) => {
+   
+    let flag = false;
+    if (a < 0){
+        a = -a;
+        flag = true;
+    }
+    if (b < 0){
+        b = -b
+    }
+    let result = a;
+    while (result >= b) {
+        result -= b;
+    }
+    if (flag) {
+        result = -result
+    }
+
+    return result;
+}
+
+const round = (value) => {
+    let product = multiply(value, 10); 
+    let mod = modulo(product, 10); 
+    if (mod >= 5 ){
+        return divide(product, 10) + 1; 
+    } else if(mod <= -5){
+        return divide(product, 10) - 1; 
+    } else {
+        return divide(product, 10); 
+    }
+}
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round)) // output: [ 4, -4, 3, -3 ]
+
+
+
+
+
+
+
