16398eeb76baefe63a98d95eeff260a51a2c8b27
hokwach <hezronokwach@gmail.com>
2024-08-29 09:18:37 +0300

    Change the file name

--- keep-cut.js
+++ keep-cut.js
@@ -1,9 +1,6 @@
 const cutFirst = (str) => str.substring(2);
-const cutLast = (str) => str.substring(0,str.length-2);
+const cutLast = (str) => str.substring(0, str.length - 2);
 const cutFirstLast = (str) => keepFirst(str) + keepLast(str);
 const keepFirst = (str) => str.substring(0, 2);
-const keepLast = (str) => str.substring(str.length-2, str.length);
-const keepFirstLast =(str) => keepFirst(str) + keepLast(str);
-
-const str = "hello00";
-console.log(keepFirstLast(str));
+const keepLast = (str) => str.substring(str.length - 2, str.length);
+const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
