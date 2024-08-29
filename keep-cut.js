ca05971c72e57bda7fae840f84ced83bd8d87237
hokwach <hezronokwach@gmail.com>
2024-08-29 09:33:09 +0300

    Change the logic in keppfirstLast function

--- keep-cut.js
+++ keep-cut.js
@@ -3,5 +3,11 @@ const cutLast = (str) => str.substring(0, str.length - 2);
 const cutFirstLast = (str) => str.substring(2,str.length-2);
 const keepFirst = (str) => str.substring(0, 2);
 const keepLast = (str) => str.substring(str.length - 2, str.length);
-const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
-console.log(keepFirstLast('abcdef'))
+const keepFirstLast = (str) => {
+    if (str.length < 4){
+        return ""
+    }else{
+        return keepFirst(str) + keepLast(str);
+    }
+}
+console.log(keepFirstLast('af'))
