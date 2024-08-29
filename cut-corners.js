89e47ae315c2add1a4ef4f63501bb9eccf10d914
hokwach <hezronokwach@gmail.com>
2024-08-29 16:04:19 +0300

    refactor:Change the logic of all the functions

--- cut-corners.js
+++ cut-corners.js
@@ -1,141 +1,113 @@
-// const multiply = (a, b) => {
-//     let result = 0;
-//     let absA = Math.abs(a)
-//     let absB = Math.abs(b)
-//     let isNegative = false;
-//     if (a < 0 && b >= 0) {
-//         isNegative = true;
-//     } else if (a >= 0 && b < 0) {
-//         isNegative = true;
-//     }
-//     while (absB > 0) {
-//         result += absA
-//         absB--
-//     }
-//     if (isNegative) {
-//         return -result
-//     } else {
-//         return result
-//     }
-// }
-
-const divide = (a, b) => {
-    if (b === 0) {
-        throw new Error("Division by zero is not allowed.");
+function round(n) {
+    if (n === 0) {
+        return 0
+    } else if (n === -Infinity) {
+        return -Infinity
+    } else if (n === Infinity) {
+        return Infinity
     }
+    let isNegative = false;
+    if (n < 0) {
+        n = -n;
+        isNegative = true;
+    }
+    
+    let intPart = floor(n);
+    let fractPart = n - intPart;
+    
+    if (fractPart >= 0.5) {
+        intPart++;
+    }
+    
+    let result = intPart;
+    if (isNegative) {
+        result = -result;
+    }
+    return result;
+}
+
+function ceil(n) {
+    if (n === 0) {
+        return 0
+    } else if (n === -0) {
+        return -0
+    } else if (n === -Infinity) {
+        return -Infinity
+    } else if (n === Infinity) {
+        return Infinity
+    }
+    let isNegative = false;    
+    let intPart = floor(n);
+    let fractPart = n - intPart;
+    
+    if (fractPart > 0) {
+        intPart++; 
+    }    
+    let result = intPart;  
+    return result;
+}
+function trunc(n) {
+    if (n === 0) {
+        return 0
+    } else if (n === -0) {
+        return -0
+    } else if (n === -Infinity) {
+        return -Infinity
+    } else if (n === Infinity) {
+        return Infinity
+    }
+    let isNegative = false;
+    if (n < 0) {
+        n = -n;
+        isNegative = true;
+    }
+    
+    let intPart = floor(n);
+    
+    let result = intPart;
+    if (isNegative) {
+        result = -result;
+    }
+    return result;
+}
+function floor(n) {
+    if (n === 0) {
+        return 0
+    } else if (n === -0) {
+        return -0
+    } else if (n === -Infinity) {
+        return -Infinity
+    } else if (n === Infinity) {
+        return Infinity
+    }
+    let isNegative = false;
+    if (n < 0) {
+        n = -n;
+        isNegative = true;
+    }
+    
     let result = 0;
-    let absA = Math.abs(a);
-    let absB = Math.abs(b);
-    let isNegative = (a < 0) ^ (b < 0);
-
-    while (absA >= absB) {
+    while (n >= 1) {
         result++;
-        absA -= absB;
+        n -= 1;
     }
-    return isNegative ? -result : result;
-}
-
-
-const modulo = (a, b) => {
-    if (b !== 0) {
-        let flag = false;
-        if (a < 0) {
-            a = -a;
-            flag = true;
+    
+    if (isNegative) {
+        if (n > 0) {
+            result = -result - 1; 
+        } else {
+            result = -result; 
         }
-        if (b < 0) {
-            b = -b
-        }
-        let result = a;
-        while (result >= b) {
-            result -= b;
-        }
-        if (flag) {
-            result = -result
-        }
-        return result;
     }
-
+    
+    return result;
 }
 
-const round = (value) => {
-    if (value === 0) {
-        return 0
-    } else if (value === -Infinity) {
-        return -Infinity
-    } else if (value === Infinity) {
-        return Infinity
-    }
-    let product = value * 10;
-    let mod = modulo(product, 10);
-    if (mod >= 5) {
-        return divide(product, 10) + 1;
-    } else if (mod <= -5) {
-        return divide(product, 10) - 1;
-    } else {
-        return divide(product, 10);
-    }
-}
-
-const floor = (value) => {
-    if (value === 0) {
-        return 0
-    } else if (value === -0) {
-        return -0
-    } else if (value === -Infinity) {
-        return -Infinity
-    } else if (value === Infinity) {
-        return Infinity
-    }
-    let product = value * 10;
-    if (value < 0) {
-        return divide(product, 10) - 1;
-    } else {
-        return divide(product, 10);
-    }
-}
-
-const trunc = (value) => {
-    if (value === 0) {
-        return 0
-    } else if (value === -0) {
-        return -0
-    } else if (value === -Infinity) {
-        return -Infinity
-    } else if (value === Infinity) {
-        return Infinity
-    }
-    let product = value * 10;
-    return divide(product, 10);
-}
-
-const ceil = (value) => {
-    if (value === 0) {
-        return 0
-    } else if (value === -0) {
-        return -0
-    } else if (value === -Infinity) {
-        return -Infinity
-    } else if (value === Infinity) {
-        return Infinity
-    }
-    let product = value * 10;
-    if (value < 0) {
-        return divide(product, 10);
-    } else {
-        return divide(product, 10) + 1;
-    }
-}
-// const nums = [3.7, -3.7, 3.1, 0]
-// console.log(nums.map(round))
-//  //console.log(nums.map(floor))
-// // console.log(nums.map(trunc))
-// //console.log(nums.map(ceil))
-
-
-
-
-
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round))
+console.log(nums.map(floor))
+console.log(nums.map(trunc))
+console.log(nums.map(ceil))
 
 
 
