b8b353edc00f52fa7146b4754a895b403fc7d94b
hokwach <hezronokwach@gmail.com>
2024-08-30 21:54:56 +0300

    Add regular expression for fin that accepts a string; returning an array with every instance of a letter, followed by a space, followed by a number, only if that number has only one digit, and is not followed by any letter.

---
+++ letter-space-number.js
@@ -0,0 +1,7 @@
+const letterSpaceNumber = (value) => {
+    const pattern = /[a-zA-Z] [0-9]\b/g
+    let result= value.match(pattern);
+    return result
+}
+console.log(letterSpaceNumber('example 1, example 20'))
+console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
