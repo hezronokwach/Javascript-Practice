bc52eeefd8fb2848b2f17f6133e7d55687e7b4c3
hokwach <hezronokwach@gmail.com>
2024-08-30 13:43:35 +0300

    refactor: Modify the method in get function

--- get.js
+++ get.js
@@ -1,19 +1,16 @@
 const get = (src, path) => {
   const keys = path.split('.');
-  let result = src;
-  
+  let result = src;  
   for (let i = 0; i < keys.length; i++) {
     if (result == null) {
-      return undefined;
-    }
-    if (i === keys.length - 1 && typeof result[keys[i]] === 'function') {
-      return keys[i];
+      return undefined; 
     }
     result = result[keys[i]];
   }  
-  return result;
+    return typeof result === 'function' ? result() : result;
 }
 
+
 // Test case
-// console.log(get({ a: [{ b: 't' }] }, 'a.0.b') === 't')
+ //console.log(get({ a: [{ b: 't' }] }, 'a.0.b') )
 // console.log(get({ a: [{ b: 't' }] }, 'a.0.b.toString')); // [Function: toString]
