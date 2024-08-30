7bb2a150fe2421b3882e31e6b4ee64155c3cce00
hokwach <hezronokwach@gmail.com>
2024-08-30 22:07:03 +0300

    refactor: Add return empty array if match not found

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,8 @@
 const letterSpaceNumber = (value) => {
     const pattern = /[a-zA-Z] [0-9]\b/g
     let result= value.match(pattern);
-    return result
+    return result || []
 }
 console.log(letterSpaceNumber('example 1, example 20'))
 console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
+console.log((letterSpaceNumber('I like 7up.')))
