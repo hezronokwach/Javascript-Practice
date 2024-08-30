927aab8beb7982b7f98a11ee6e971a5dbb544d47
hokwach <hezronokwach@gmail.com>
2024-08-30 13:57:11 +0300

    refactor: Modify the method in get function

--- get.js
+++ get.js
@@ -1,16 +1,20 @@
 const get = (src, path) => {
   const keys = path.split('.');
-  let result = src;  
-  for (let i = 0; i < keys.length; i++) {
-    if (result == null) {
-      return undefined; 
+  let result = src;
+  keys.forEach((element) => {
+    if (result === undefined){
+      return result
     }
-    result = result[keys[i]];
-  }  
-    return typeof result === 'function' ? result() : result;
+     result = result[element]
+    
+  });
+  return result
 }
-
-
-// Test case
- //console.log(get({ a: [{ b: 't' }] }, 'a.0.b') )
-// console.log(get({ a: [{ b: 't' }] }, 'a.0.b.toString')); // [Function: toString]
+  
+  console.log(get({ a: [{ b: 't' }] }, 'a.0.b.toString'))
+//   const src = { nested: { key: 'peekaboo' } };
+//   const path = 'nested.key';
+//   console.log(get(src, path)); // -> 'peekaboo'
+//   console.log(get({ key: 'value' }, 'key'))
+  
+//   console.log(get(src, 'nested')); // -> { key: 'peekaboo' }
