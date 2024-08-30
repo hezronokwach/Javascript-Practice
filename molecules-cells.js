05746e5900da6b7483291bb35bb24eb069758084
hokwach <hezronokwach@gmail.com>
2024-08-30 09:12:12 +0300

    Add RNA and DNA functions

---
+++ molecules-cells.js
@@ -0,0 +1,48 @@
+const RNA = (dna) => {
+    let result = "";
+    for (let i = 0; i < dna.length; i++) {
+        switch (dna[i]) {
+            case 'C':
+                result += 'G';
+                break;
+            case 'G':
+                result += 'C';
+                break;
+            case 'A':
+                result += 'T';
+                break;
+            case 'U':
+                result += 'A';
+                break;
+            default:
+                result = ""
+        }
+
+    }
+    return result;
+}
+
+const DNA = (rna) => {
+    let result = "";
+    for (let i = 0; i < rna.length; i++) {
+        switch (rna[i]) {
+            case 'C':
+                result += 'G';
+                break;
+            case 'G':
+                result += 'C';
+                break;
+            case 'A':
+                result += 'T';
+                break;
+            case 'U':
+                result += 'A';
+                break;
+            default:
+                result = ""
+        }
+
+    }
+    return result;
+}
+// console.log(RNA('TAGC'))
