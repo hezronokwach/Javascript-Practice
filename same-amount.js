3603f3dcfc1640edaddc4e0b367ef63584143e9c
hokwach <hezronokwach@gmail.com>
2024-08-31 10:08:11 +0300

    refactor: Change the check for matching

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,6 @@
 const sameAmount = (value, regex1 ,regex2) => {
-    regex1 = new RegExp(regex1,g);
-    regex1 = new RegExp(regex2,g);
+    regex1 = new RegExp(regex1,'g');
+    regex1 = new RegExp(regex2,'g');
     let match1 = (value.match(regex1) || []).length;
     let match2 = (value.match(regex2) || []).length;
     return match1 === match2;
