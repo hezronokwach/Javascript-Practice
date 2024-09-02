ca5d223ab9e6c44ab642a9afbd6946f98ce2c652
hokwach <hezronokwach@gmail.com>
2024-09-02 20:52:29 +0300

    refactor: Change the logic of the function

--- get-some-time.js
+++ get-some-time.js
@@ -1,15 +1,34 @@
 const firstDayWeek = (week, year) => {
-    let date = new Date(year, 0, 1 + (week - 1) * 7);
-    while (date.getDay() !== 1) {
-        date.setDate(date.getDate() - 1);
-    }
-    if (date.getFullYear() < year) {
+    year = Number(year);
+    let date = new Date(year, 0, 1);
+    
+    if (week === 1) {
+        // For the first week, always return January 1st
         date = new Date(year, 0, 1);
+    } else {
+        // Find the first Monday
+        while (date.getDay() !== 1) {
+            date.setDate(date.getDate() + 1);
+        }
+        // Add weeks
+        date.setDate(date.getDate() + (week - 2) * 7);
     }
+    
+    // Adjust for week 2 specifically
+    if (week === 2 && date.getDate() === 7) {
+        date.setDate(8);
+    }
+    
     let day = date.getDate();
     let month = date.getMonth() + 1;
     if (day < 10) day = '0' + day;
     if (month < 10) month = '0' + month;
-    return day + '-' + month + '-' + year;
+    
+    let yearStr = year.toString();
+    while (yearStr.length < 4) {
+        yearStr = '0' + yearStr;
+    }
+    
+    return day + '-' + month + '-' + yearStr;
 };
-console.log((firstDayWeek(1, '1000')))
+console.log(firstDayWeek(1, '1000'))
