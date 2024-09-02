1d06e64dea87fb80926b66816540dbd525f9876d
hokwach <hezronokwach@gmail.com>
2024-09-02 20:14:01 +0300

    refactor: Modify all function to check if the arguments are not strings

--- date-is.js
+++ date-is.js
@@ -1,12 +1,27 @@
 const isValid = (date) => {
     if (typeof date === 'string') return false;
     return !isNaN(new Date(date).getTime());
-}; 
-const isAfter= (date1,date2) => (new Date(date1)) > new Date(date2) ? true : false
-const isBefore= (date1,date2) => (new Date(date1)) < new Date(date2) ? true : false
-const isFuture = (date) => new Date(date) > new Date();
-const isPast = (date) => new Date(date) < new Date();
- console.log(isValid('2013-01-01'))
+};
+
+const isAfter = (date1, date2) => {
+    if (typeof date1 === 'string' || typeof date2 === 'string') return false;
+    return new Date(date1) > new Date(date2);
+};
+
+const isBefore = (date1, date2) => {
+    if (typeof date1 === 'string' || typeof date2 === 'string') return false;
+    return new Date(date1) < new Date(date2);
+};
+
+const isFuture = (date) => {
+    if (typeof date === 'string') return false;
+    return new Date(date) > new Date();
+};
+
+const isPast = (date) => {
+    if (typeof date === 'string') return false;
+    return new Date(date) < new Date();
+};
 // console.log(isAfter(new Date(2321, 11, 21), new Date(Date.now())))
 // console.log(isAfter(new Date('2157-11-07'), new Date('2183-04-16')))
 // console.log(isFuture(new Date('1992-01-01')))
