1c8a325d8658f07253a65b6778532c6515cc4a4f
hokwach <hezronokwach@gmail.com>
2024-09-02 20:36:46 +0300

    refactor: Change the return format

--- get-some-time.js
+++ get-some-time.js
@@ -4,7 +4,12 @@ const firstDayOfWeek = (week, year) => {
         date.setDate(date.getDate() - 1);
     }
     if (date.getFullYear() < year) {
-        return new Date(year, 0, 1).getDate();
+        date = new Date(year, 0, 1);
     }
-    return date.getDate();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    return day + '-' + month + '-' + year;
 };
+console.log(firstDayOfWeek(4, 2022)); // 04-
