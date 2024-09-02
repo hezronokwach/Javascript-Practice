d1d24e1d8638571aed8bfcf23adca29740e1cada
hokwach <hezronokwach@gmail.com>
2024-09-02 20:55:52 +0300

    refactor: Change the logic of the function

--- get-some-time.js
+++ get-some-time.js
@@ -1,34 +1,21 @@
-const firstDayWeek = (week, year) => {
-    year = Number(year);
+function firstDayWeek(week, year) {
     let date = new Date(year, 0, 1);
-    
     if (week === 1) {
-        // For the first week, always return January 1st
-        date = new Date(year, 0, 1);
-    } else {
-        // Find the first Monday
-        while (date.getDay() !== 1) {
-            date.setDate(date.getDate() + 1);
-        }
-        // Add weeks
-        date.setDate(date.getDate() + (week - 2) * 7);
+        return formatDate(date);
     }
     
-    // Adjust for week 2 specifically
-    if (week === 2 && date.getDate() === 7) {
-        date.setDate(8);
+    date.setDate(date.getDate() + (week - 1) * 7);
+    while (date.getDay() !== 1) {
+        date.setDate(date.getDate() - 1);
     }
     
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    if (day < 10) day = '0' + day;
-    if (month < 10) month = '0' + month;
-    
-    let yearStr = year.toString();
-    while (yearStr.length < 4) {
-        yearStr = '0' + yearStr;
-    }
-    
-    return day + '-' + month + '-' + yearStr;
-};
+    return formatDate(date);
+}
+
+function formatDate(date) {
+    let day = String(date.getDate()).padStart(2, '0');
+    let month = String(date.getMonth() + 1).padStart(2, '0');
+    let year = String(date.getFullYear()).padStart(4, '0');
+    return `${day}-${month}-${year}`;
+}
 console.log(firstDayWeek(1, '1000'))
