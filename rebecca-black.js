6a41900f2e14843287d6e4a2d41a01145811452d
hokwach <hezronokwach@gmail.com>
2024-09-03 10:55:04 +0300

    refactor: Change the check for leap year

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,6 +1,9 @@
 const isFriday = (date) => (date).getDay() === 5;
 const isWeekend = date => date.getDay() % 6 === 0;
-const isLeapYear = date => (date.getFullYear(), 1, 29 === 29);
+const isLeapYear = date => {
+    const year = date.getFullYear();
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+};
 const isLastDayOfMonth = (date = new Date()) => date.getDate() === new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 // console.log(isFriday(new Date('2014-08-29')))
 // console.log(isLeapYear(new Date('2000-02-29')))
