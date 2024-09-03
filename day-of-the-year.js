25f9a31e729687edc23319ccc99d5de3b99f3a31
hokwach <hezronokwach@gmail.com>
2024-09-03 14:24:37 +0300

    Add function to return the number of days since the start of the year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,20 @@
 const dayOfTheYear = (date) => {
-    let firstDay = new Date(date.getFullYear(), 0 ,0);
-    return Math.floor((new Date(date) -firstDay)/(1000 * 60 * 60 * 24))
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        daysInMonth[1] = 29;
+    }
+    
+    let dayCount = day;
+    for (let i = 0; i < month; i++) {
+        dayCount += daysInMonth[i];
+    }
+    
+    return dayCount;
 }
 
 console.log(dayOfTheYear(new Date('1600-12-31')))
