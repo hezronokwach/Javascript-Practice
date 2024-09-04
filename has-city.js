c3022abf99d780ee86f7a602b2a86a824e550949
hokwach <hezronokwach@gmail.com>
2024-09-04 19:36:06 +0300

    fix: Change the order of arguments

--- has-city.js
+++ has-city.js
@@ -1,8 +1,10 @@
-const hasCity = (arr,country) => (city) => (arr.includes(city)) ? `${city} is a city from ${country}` :`${city} is not a city from ${country}`
-// const cities = ['New York', 'London', 'Paris', 'Tokyo'];
+const hasCity = (country, arr) => (city) => arr.includes(city) ? `${city} is a city from ${country}` : `${city} is not a city from ${country}`;// const cities = ['New York', 'London', 'Paris', 'Tokyo'];
 // const isFrenchCity = hasCity(cities, 'France');
 
 // console.log(isFrenchCity('Paris')); // Output: Paris is a city from France
 // console.log(isFrenchCity('Berlin')); // Output: Berlin is not a city from France
 
     
+const chineseCities = ['Beijing', 'Shanghai', 'Wuhan', 'Shenzhen', 'Tianjin', 'Chengdu'];
+const isChinese = hasCity(chineseCities, 'China');
+console.log(isChinese('Beijing'))
