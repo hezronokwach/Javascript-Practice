e69cf448f848fed61d69341d4a0394bfb569a9fc
hokwach <hezronokwach@gmail.com>
2024-09-06 13:41:00 +0300

    refactor: Modify the count of elements

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,26 +1,18 @@
 export const generateLetters = () => {
     let body = document.querySelector('body')
-    let count = 0;
-    const addLetters = () => {
-        if (count <= 120) {
-            count++;
-            let letters = document.createElement('div');
-            letters.textContent = createRandomString();
-            letters.style.fontSize = `${11 + count}px`;
-            if (count <= 40) {
-                letters.style.fontWeight = '300';
-            } else if (count > 40 && count <= 80) {
-                letters.style.fontWeight = '400';
-            } else {
-                letters.style.fontWeight = '600';
-            }
-            body.appendChild(letters);
-            setTimeout(addLetters, 0);
+    for (let count = 1; count <= 120; count++) {
+        let letters = document.createElement('div');
+        letters.textContent = createRandomString();
+        letters.style.fontSize = `${11 + count}px`;
+        if (count <= 40) {
+            letters.style.fontWeight = '300';
+        } else if (count > 40 && count <= 80) {
+            letters.style.fontWeight = '400';
+        } else {
+            letters.style.fontWeight = '600';
         }
-    };
-
-    addLetters();
-
+        body.appendChild(letters);
+    }
 }
 function createRandomString() {
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
