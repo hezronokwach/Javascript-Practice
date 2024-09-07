b520d332826b821173b9a494182992df51587b85
hokwach <hezronokwach@gmail.com>
2024-09-07 10:49:06 +0300

    Remove unnecessary file

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,32 @@
+export const generateLetters = () => {
+    let body = document.querySelector('body')
+    let count = 0;
+    const addLetters = () => {
+        if (count < 120) {
+            count++;
+            let letters = document.createElement('div');
+            letters.textContent = createRandomString();
+            letters.style.fontSize = `${11 + count}px`;
+            if (count <= 40) {
+                letters.style.fontWeight = '300';
+            } else if (count > 40 && count <= 80) {
+                letters.style.fontWeight = '400';
+            } else {
+                letters.style.fontWeight = '600';
+            }
+            body.appendChild(letters);
+            setTimeout(addLetters, 0);
+        }
+    };
+
+    addLetters();
+
+}
+function createRandomString() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    result = chars.charAt(Math.floor(Math.random() * chars.length));
+    return result;
+}
+
+
