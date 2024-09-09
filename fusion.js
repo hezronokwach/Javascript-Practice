342afcab8848f4bec422e208d0e2cfc838fee909
hokwach <hezronokwach@gmail.com>
2024-09-09 19:49:32 +0300

    fix: Add brackets after checking the first condition in the array type

--- fusion.js
+++ fusion.js
@@ -2,7 +2,7 @@ const fusion = (obj1, obj2) => {
     let result = {}
     for (const key of [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])]) {
         if (key in obj1 && key in obj2) {
-            if (Array.isArray(obj1[key] && Array.isArray(obj2[key]))) {
+            if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
                 result[key] = [...obj1[key], ...obj2[key]]
             } else if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
                 result[key] = fusion(obj1[key], obj2[key])
@@ -21,3 +21,5 @@ const fusion = (obj1, obj2) => {
     }
     return result;
 }
+
+console.log(fusion({ arr: [1, '2'] }, { arr: [2] }))
