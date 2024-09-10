182bec061faa51f07dc7e5e9b084125f0a87cdc6
hokwach <hezronokwach@gmail.com>
2024-09-10 18:59:57 +0300

    fix: Add check for function and regex types

--- deep-copy.js
+++ deep-copy.js
@@ -7,6 +7,14 @@ const deepCopy = (obj) => {
         return obj.map(deepCopy);
     }
     
+    if (obj instanceof RegExp) {
+        return new RegExp(obj);
+    }
+    
+    if (typeof obj === 'function') {
+        return obj;
+    }
+    
     const copy = {};
     for (const key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
