bd21c073d1df390e772a3cf05497037556329250
hokwach <hezronokwach@gmail.com>
2024-09-10 20:16:45 +0300

    fix: Rename function name to pronoun

--- pronoun.js
+++ pronoun.js
@@ -1,15 +1,15 @@
-const pronouns = (str) => {
-    let pronoun = ["i", "you", "he", "she", "it", "they", "we"];
+const pronoun = (str) => {
+    let prononuns = ["i", "you", "he", "she", "it", "they", "we"];
     let result = {};
     let words = str.split(' ');
     for (let i = 0; i < words.length; i++) {
         let word = words[i].toLowerCase();
-        if (pronoun.includes(word)) {
+        if (prononuns.includes(word)) {
             if (!result.hasOwnProperty(word)) {
                 result[word] = {word: [], count: 0};
             }
             result[word].count++;
-            if (i + 1 < words.length && !pronoun.includes(words[i+1].toLowerCase())) {
+            if (i + 1 < words.length && !prononuns.includes(words[i+1].toLowerCase())) {
                 if (!result[word].word.includes(words[i+1])) {
                     result[word].word.push(words[i+1]);
                 }
@@ -20,4 +20,4 @@ const pronouns = (str) => {
 }
 
 const ex = 'If he you want to buy something you have to pay.'
-console.log(pronouns(ex))
+console.log(pronoun(ex))
