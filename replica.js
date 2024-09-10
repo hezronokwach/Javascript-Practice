9bb30b250ff907044ac26d90d1b7d9b59970fa07
hokwach <hezronokwach@gmail.com>
2024-09-10 19:06:43 +0300

    Add function thats allows deep assign values of all properties from one or more objects to a target object

---
+++ replica.js
@@ -0,0 +1,14 @@
+function replica(target, ...sources) {
+    for (const source of sources) {
+        for (const key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                if (source[key] !== null && typeof source[key] === 'object') {
+                    target[key] = replica(Array.isArray(source[key]) ? [] : {}, target[key], source[key]);
+                } else {
+                    target[key] = source[key];
+                }
+            }
+        }
+    }
+    return target;
+}
