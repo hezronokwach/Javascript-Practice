42e1ea8e35b7b5b2e78ae6a1badcb7bdfefb5d86
hokwach <hezronokwach@gmail.com>
2024-09-10 19:07:58 +0300

    refactor: Add check for regex types

--- replica.js
+++ replica.js
@@ -2,7 +2,9 @@ function replica(target, ...sources) {
     for (const source of sources) {
         for (const key in source) {
             if (Object.prototype.hasOwnProperty.call(source, key)) {
-                if (source[key] !== null && typeof source[key] === 'object') {
+                if (source[key] instanceof RegExp) {
+                    target[key] = new RegExp(source[key]);
+                } else if (source[key] !== null && typeof source[key] === 'object') {
                     target[key] = replica(Array.isArray(source[key]) ? [] : {}, target[key], source[key]);
                 } else {
                     target[key] = source[key];
