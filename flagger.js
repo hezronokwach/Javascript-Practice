638037f19df067e19b47a9d5f6e92865cdf5bddd
hokwach <hezronokwach@gmail.com>
2024-09-11 19:36:13 +0300

    Add newline between the outputs

--- flagger.js
+++ flagger.js
@@ -1,24 +1,24 @@
 const flags = (obj) => {
     let output = { alias: { h: 'help' }, description: "" }
     let flagDescription = {}
-    
+
     for (let key in obj) {
         output.alias[Alias(key)] = key
         if (key !== 'help') {
             flagDescription[key] = obj[key]
         }
     }
-    
+
     if (!obj.hasOwnProperty('help') || obj.help === '') {
         output.description = Object.entries(flagDescription)
             .map(([key, desc]) => `-${Alias(key)}, --${key}: ${desc}`)
-            .join('')
+            .join('\n')
     } else if (Array.isArray(obj.help)) {
-        output.description = obj.help.map(key => 
+        output.description = obj.help.map(key =>
             `-${Alias(key)}, --${key}: ${flagDescription[key]}`
-        ).join('')
+        ).join('\n')
     }
-    
+
     return output
 }
 
@@ -27,9 +27,9 @@ function Alias(word) {
 }
 
 
-console.log( flags({
+console.log(flags({
     invert: 'inverts and object',
     'convert-map': 'converts the object to an array',
     assign: 'uses the function assign - assign to target object',
     help: ['assign', 'invert'],
-  }))
+}))
