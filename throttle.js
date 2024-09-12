c7ade7b82331618109881d189949243258691a11
hokwach <hezronokwach@gmail.com>
2024-09-12 17:55:06 +0300

    Add functions that will work like _.throttle from lodash.

---
+++ throttle.js
@@ -0,0 +1,41 @@
+const throttle = (func, wait) => {
+    let lastCall = 0
+    return function(...args){
+        let current = Date.now();
+        if((current - lastCall) >= wait){
+            func.apply(this, args)
+            lastCall = current
+        }
+
+    }
+}
+
+const opThrottle = (func, wait, options = {}) => {
+    let lastCall = 0;
+    let lastArgs = null;
+    let timer = null;
+
+    return function(...args) {
+        const current = Date.now();
+
+        if (options.leading && lastCall === 0) {
+            func.apply(this, args);
+            lastCall = current;
+        } else if (current - lastCall >= wait) {
+            func.apply(this, args);
+            lastCall = current;
+        } else if (options.trailing) {
+            lastArgs = args;
+            if (!timer) {
+                timer = setTimeout(() => {
+                    if (lastArgs) {
+                        func.apply(this, lastArgs);
+                        lastCall = Date.now();
+                        lastArgs = null;
+                        timer = null;
+                    }
+                }, wait - (current - lastCall));
+            }
+        }
+    };
+};
