2e4a8ad9a39387b7b7f1f5a43cca726d485b623c
hokwach <hezronokwach@gmail.com>
2024-09-13 21:23:42 +0300

    Add function that interpolates points from the start position to the end position

---
+++ interpolation.js
@@ -0,0 +1,24 @@
+function interpolation(obj) {
+    let step = obj.step
+    let start = obj.start
+    let end = obj.end
+    let callback = obj.callback
+    let duration = obj.duration
+    interval = duration/step
+    let decrement = (end - start)/step
+  for (let i = 0; i < step; i++) {
+    let distance = start + (i * decrement)
+    let time = i * interval
+    
+    setTimeout(() => {
+        callback([distance, time])
+    }, time)
+}
+}
+interpolation({
+    step: 5,
+    start: 0,
+    end: 1,
+    duration: 10,
+    callback: (point) => console.log(point)
+});
