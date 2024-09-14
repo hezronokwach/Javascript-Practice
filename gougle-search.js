edfc23fafc85390ef387df4ccac7ed43d7ccb328
hokwach <hezronokwach@gmail.com>
2024-09-14 11:18:51 +0300

    fix: Add queryServers function

--- gougle-search.js
+++ gougle-search.js
@@ -1,3 +1,9 @@
+async function queryServers(serverName, q) {
+    const url = `/${serverName}?q=${q}`;
+    const backupUrl = `/${serverName}_backup?q=${q}`;
+    return Promise.race([getJSON(url), getJSON(backupUrl)]);
+}
+
 async function gougleSearch(q) {
     const timeout = new Promise((_, reject) =>
         setTimeout(() => reject(new Error('timeout')), 80)
