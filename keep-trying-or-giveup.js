d377f999be6f91d49d9bacc9e0f0e743bdb923e8
hokwach <hezronokwach@gmail.com>
2024-09-14 10:06:14 +0300

    Add functions to demonstrate async function

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,27 @@
+function retry(count, callback) {
+    return function (...args) {
+        for (let i = 0; i < count; i++) {
+            try {
+                return callback(...args)
+            }
+            catch (error) {
+                if (i === count - 1) {
+                    throw new Error('All attempts failed');
+                }
+            }
+
+        }
+    }
+}
+
+function timeout(delay, callback) {
+    return async function (...args) {
+        let timeoutPromise = new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('timeout')), delay)
+        );
+        return Promise.race([
+            callback(...args),
+            timeoutPromise
+        ]);
+    };
+}
