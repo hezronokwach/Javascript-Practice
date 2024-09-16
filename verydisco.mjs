68d1a6bc04268df73e2cdebaf59450408846f3b0
hokwach <hezronokwach@gmail.com>
2024-09-16 16:05:25 +0300

    Add function that read command arguments and switches the order of arguments

---
+++ verydisco.mjs
@@ -0,0 +1,19 @@
+import { argv } from 'node:process';
+let args
+if (argv.length < 3) {
+    args = argv.slice(2);
+} else {
+    args = argv.slice(2, 3)
+}
+function myDisco(word) {
+    let middle = Math.ceil(word.length / 2);
+    let first = word.slice(0, middle);
+    let end = word.slice(middle);
+    return end + first;
+}
+
+function processInput(input) {
+    return input.map(word => myDisco(word)).join(' ');
+}
+
+console.log(processInput(args));
