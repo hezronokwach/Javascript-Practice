c8f060b418456ef2ac129744abbec2c9e0a3755f
hokwach <hezronokwach@gmail.com>
2024-09-16 19:05:36 +0300

    Add function that reads the guests list and writes to file the guest with answer with yes

---
+++ tell-me-vip.mjs
@@ -0,0 +1,49 @@
+import { writeFile } from 'node:fs/promises';
+import { readFile } from 'node:fs/promises';
+import { readdir } from 'node:fs/promises';
+import { argv } from 'node:process'
+
+let filePath = argv[2]
+let files
+let store = []
+let list = ""
+try {
+    files = await readdir(filePath);
+    for (let i = 0; i < files.length; i++) {
+        let content = await readFile(`${filePath}/${files[i]}`, 'utf8')
+        let obj = (JSON.parse(content))
+        if (obj.answer === 'yes') {
+            store.push(files[i])
+        }
+    }
+    let result = split(store)
+    result.sort()
+    //  console.log(result)
+    for (let i = 0; i < result.length; i++) {
+        
+        list +=(`${i + 1}. ${result[i]}`) + "\n"
+    }
+    writeFile('vip.txt',list )
+
+    //list += result.slice(result.length-1)
+    //console.log(list)
+} catch (err) {
+    console.error(err);
+}
+//console.log(files)
+
+function split(arr) {
+    let result = []
+    for (let file of arr) {
+        let store = file.split('_')
+        store[1] = store[1].slice(0, -5)
+        let conc = store[1] + " " + store[0]
+        result.push(conc)
+
+    }
+    return result
+}
+
+//console.log(split(['Dotty_Dunlap.json']))
+// let path = process.cwd()
+// console.log(path)
