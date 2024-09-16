2c5192c47e73c4dcc4e01980d59cc16d50ef7911
hokwach <hezronokwach@gmail.com>
2024-09-16 18:22:59 +0300

    Add function that read the path provided in the arguments and formats the output

---
+++ tell-me-who.mjs
@@ -0,0 +1,33 @@
+import { readdir } from 'node:fs/promises';
+import { argv } from 'node:process'
+
+let filePath=argv[2]
+let files
+try {
+    files = await readdir(filePath);
+    files.sort()
+    let result = split(files)
+  //  console.log(result)
+    for (let i = 0; i < result.length; i++) {
+        console.log(`${i+1}. ${result[i]}`)
+    }
+} catch (err) {
+    console.error(err);
+}
+//console.log(files)
+
+function split(arr) {
+    let result = []
+    for (let file of arr) {
+        let store = file.split('_')
+        store[1] = store[1].slice(0, -5)
+        let conc = store[1] + " " + store[0]        
+        result.push(conc) 
+        
+    }
+    return result
+}
+
+//console.log(split(['Dotty_Dunlap.json']))
+// let path = process.cwd()
+// console.log(path)
