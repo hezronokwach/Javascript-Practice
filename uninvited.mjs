59605ea2ee86b77dbe3f2a90a83e920085d9675c
hokwach <hezronokwach@gmail.com>
2024-09-16 20:58:59 +0300

    Add function that uses a serer to modify a JSON file for guests remotely

---
+++ uninvited.mjs
@@ -0,0 +1,37 @@
+import { createServer } from 'node:http';
+import { writeFile } from 'node:fs';
+let port = 5000
+
+const server = createServer((request, response) => {
+    const url = new URL(request.url, `http://${request.headers.host}`);
+    const guestName = url.pathname.slice(1);
+    response.setHeader('Content-Type', 'application/json');
+    if (request.method === 'POST') {
+        let body = '';
+        request.on('data', chunk => body += chunk);
+        request.on('end', () => {
+            try {
+                const jsonContent = JSON.parse(body);
+                writeFile(`guests/${guestName}.json`, JSON.stringify(jsonContent), (err) => {
+                    if (err) {
+                        response.statusCode = 500;
+                        response.end(JSON.stringify({ error: "server failed" }));
+                    } else {
+                        response.statusCode = 201;
+                        response.end(JSON.stringify(jsonContent));
+                    }
+                });
+            } catch (error) {
+                response.statusCode = 400;
+                response.end(JSON.stringify({ error: "Invalid JSON" }));
+            }
+        });
+    } else {
+        response.statusCode = 405;
+        response.end(JSON.stringify({ error: "wrong method" }));
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server started on localhost:${port}!`);
+});
