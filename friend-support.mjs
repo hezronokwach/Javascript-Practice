ee83a7437ff1253dbfbdc005e371a08b9ff14836
hokwach <hezronokwach@gmail.com>
2024-09-16 20:32:20 +0300

    fix: Change so that the server looks for the guest folder

--- friend-support.mjs
+++ friend-support.mjs
@@ -3,29 +3,29 @@ import { readFile } from 'node:fs/promises';
 
 let port = 5000
 const server = createServer(async (request, response) => {
-  const url = new URL(request.url, `http://${request.headers.host}`);
-  const guestName = url.pathname.slice(1);
-  response.setHeader('Content-Type', 'application/json');
+    const url = new URL(request.url, `http://${request.headers.host}`);
+    const guestName = url.pathname.slice(1);
+    response.setHeader('Content-Type', 'application/json');
 
-  try {
-    if (guestName) {
-      const content = await readFile(`${guestName}.json`, 'utf8');
-      const jsonContent = JSON.parse(content);
-      response.statusCode = 200;
-      response.end(JSON.stringify(jsonContent));
-    } else {
-      response.statusCode = 404;
-      response.end(JSON.stringify({ error: "guest not found" }));
+    try {
+        if (guestName) {
+            const content = await readFile(`guests/${guestName}.json`, 'utf8');
+            const jsonContent = JSON.parse(content);
+            response.statusCode = 200;
+            response.end(JSON.stringify(jsonContent));
+        } else {
+            response.statusCode = 404;
+            response.end(JSON.stringify({ error: "guest not found" }));
+        }
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            response.statusCode = 404;
+            response.end(JSON.stringify({ error: "guest not found" }));
+        } else {
+            response.statusCode = 500;
+            response.end(JSON.stringify({ error: "server failed" }));
+        }
     }
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      response.statusCode = 404;
-      response.end(JSON.stringify({ error: "guest not found" }));
-    } else {
-      response.statusCode = 500;
-      response.end(JSON.stringify({ error: "server failed" }));
-    }
-  }
 });
 
 server.listen(5000, () => {
